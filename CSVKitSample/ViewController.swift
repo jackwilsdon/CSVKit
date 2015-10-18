a7864d42b60743240f6b4f794df5cb4adbb2882c
Jack Wilsdon <jack.wilsdon@gmail.com>
2015-10-18 01:24:47 +0100

    Add CSVKit code

---
+++ CSVKitSample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CSVKitSample
+//
+//  Created by Jack Wilsdon on 18/10/2015.
+//  Copyright © 2015 Jack Wilsdon. All rights reserved.
+//
+
+import UIKit
+import CSVKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
